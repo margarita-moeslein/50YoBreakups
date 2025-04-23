352722859bec9c98bf0910e117c42e61675e4490
Margarita Möslein <106144656+margarita-moeslein@users.noreply.github.com>
2025-04-23 15:23:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
+from twilio_api import send_whatsapp_message, fetch_latest_messages
+import time
+
+
+def set_interval(fn, sec):
+    while True:
+        time.sleep(sec)
+        fn()
+
+
+def breakup():
+    print("break up all")
+
 def main():
-    pass
+    message_to_send = input("Enter message to send via WhatsApp: ")
+    send_whatsapp_message(message_to_send)
+    fetch_latest_messages()
 
 
 if __name__ == "__main__":
+    # set_interval(breakup, 10)
     main()
