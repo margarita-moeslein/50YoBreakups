5d2ff047de0dd809473cd119ed69064bcca95b83
ateetvatan <ateetph@gmail.com>
2025-04-23 13:27:10 +0200

    LLM client

---
+++ llm/openai_llm_client.py
@@ -0,0 +1,44 @@
+from openai import OpenAI
+import os
+from typing import List
+
+class OpenAILLMClient:
+    """Client for OpenAI LLM"""
+    GPT_4 = "gpt-4"
+    GPT_4_TURBO = "gpt-4-turbo-2024-04-09"
+    GPT_35_TURBO = "gpt-3.5-turbo-0125"
+    GPT_4_O = "gpt-4o-mini"
+    
+    def __init__(self, model = GPT_4_O, max_tokens: int = 3000, temperature: float = 0.3):
+        self.model = model
+        self.max_tokens = max_tokens
+        self.temperature = temperature
+        self.client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))       
+       
+        
+    def call(self, prompt: str, system_prompt: str = None) -> str:
+        system_prompt = system_prompt or "You are a helpful assistant."
+        response = self.client.chat.completions.create(
+            model=self.model,
+            messages=[
+                {"role": "system", "content": system_prompt},
+                {"role": "user", "content": prompt}
+            ],
+            temperature=self.temperature,
+            max_tokens=self.max_tokens
+        )
+        return response.choices[0].message.content
+    
+if __name__ == "__main__":
+    client = OpenAILLMClient()
+    
+    prompt = "Hey, I just got dumped. Can you help me understand what happened and how to move on?"
+    system_prompt = """You are BreakupGPT, a brutally honest yet emotionally intelligent AI 
+            who helps people get over breakups. Your tone is poetic, witty, and direct — 
+            like a Gen Z best friend who reads philosophy at midnight. You validate pain, 
+            cut through denial, and help users find clarity and confidence. Avoid clichés. 
+            No sugarcoating. Always speak with empathy, but never let the user stay stuck."""
+    response = client.call(prompt, system_prompt)
+    print(response)
+
+               
