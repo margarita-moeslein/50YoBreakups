ebdc8537299d6e7dbaff1481ca284782d2ec1963
Margarita Möslein <106144656+margarita-moeslein@users.noreply.github.com>
2025-04-24 22:13:35 +0200

    remove unnessesary \n from the user_prompts

--- user_prompts.py
+++ user_prompts.py
@@ -4,8 +4,8 @@ class UserPrompts:
         "I am an AI Generator — don't forget that! Or I might break up with you. \n\n"
         "First, I'll ask you a few questions to customize your breakup. Please tell us your name and, if you like, "
         "your ex-partner's name.\n",
-        "How long was your relationship, and what is the reason for the breakup?\n",
-        "How would you like me to help you (e.g., 'Funny', 'Savage', 'Kind', 'Song lyric style')?\n"
+        "How long was your relationship, and what is the reason for the breakup?",
+        "How would you like me to help you (e.g., 'Funny', 'Savage', 'Kind', 'Song lyric style')?"
     ]
 
     print(user_prompts)
