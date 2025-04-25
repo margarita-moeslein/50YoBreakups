4fec9aa22edf3bfcae59518737f9e3e8b4bd9ef4
markusbeermann <Markus_Beermann@icloud.com>
2025-04-25 03:01:08 +0200

    images

---
+++ beautify_json.py
@@ -0,0 +1,21 @@
+import json
+
+def beautify_json_response(json_str):
+    data = json.loads(json_str)
+    lines = []
+    for key, value in data.items():
+        lines.append(f"{key}: {value}")
+    return "\n\n".join(lines)
+
+# Test structure
+json_str = '''{
+  "serious_ideas": "Ernie, it’s time to face the music. ...",
+  "serious_ideas_song": "The Night We Met by Lord Huron ...",
+  "funny_ideas": "Hey Bert, remember that time ...",
+  "funny_ideas_song": "I Will Survive by Gloria Gaynor ...",
+  "wired_ideas": "How about we both take a spontaneous trip ...",
+  "song_idea": "Breakup Song by Frances ...",
+  "wired_ideas_song": "We Are Never Ever Getting Back Together by Taylor Swift ..."
+}'''
+
+print(beautify_json_response(json_str))
